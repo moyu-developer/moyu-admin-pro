f3c35a542e881d76b36dc52b5cd99a2d82ab7edc
wangly19 <18607948581@163.com>
2022-09-11 23:44:02 +0800

    feat: Initialize editor subapplication 🎉

---
+++ app/editor/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import yayJpg from '../assets/yay.jpg';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h2>Yay! Welcome to umi!</h2>
+      <p>
+        <img src={yayJpg} width="388" />
+      </p>
+      <p>
+        app1
+      </p>
+    </div>
+  );
+}
