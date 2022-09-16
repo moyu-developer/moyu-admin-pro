ef339e3a74eb42734da2397c7e1842d27c8884fa
wangly19 <18607948581@163.com>
2022-09-16 23:37:35 +0800

    chore: Initialization rootContainer hook .

---
+++ app/root/src/app.tsx
@@ -0,0 +1,10 @@
+
+import { ChakraProvider } from '@chakra-ui/react'
+
+export function rootContainer(container: any) {
+  
+  return (<ChakraProvider>
+    {container}
+    </ChakraProvider>
+  )
+}
