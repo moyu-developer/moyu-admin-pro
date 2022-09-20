3f802112094f77aa481bdd931055e52ab9454803
wangly19 <18607948581@163.com>
2022-09-20 23:52:13 +0800

    chore: add dashboard page.

--- app/root/src/pages/dashboard/BaseStarCard.tsx
+++ app/root/src/pages/dashboard/BaseStarCard.tsx
@@ -1,28 +1,54 @@
-import { Box, Heading, Stack, Text, useBreakpointValue, useColorModeValue } from '@chakra-ui/react'
+import {
+  Box,
+  Flex,
+  Heading,
+  Stack,
+  Text,
+  useBreakpointValue,
+  useColorModeValue,
+  IconButton,
+  Icon,
+} from "@chakra-ui/react";
+import { IconSearch } from "@tabler/icons";
 
-interface Props {
-  label: string
-  value: string
+interface BaseStatCardProps {
+  label: string;
+  value: string;
 }
-const BaseStatCard = (props: Props) => {
-  const { label, value, ...boxProps } = props
+const BaseStatCard = (props: BaseStatCardProps) => {
+  const { label, value, ...boxProps } = props;
   return (
-    <Box
-      px={{ base: '4', md: '6' }}
-      py={{ base: '5', md: '6' }}
+    <Flex
+      px={{ base: "4", md: "6" }}
+      py={{ base: "5", md: "6" }}
       bg="white"
       borderRadius="lg"
-      boxShadow={useColorModeValue('sm', 'sm-dark')}
+      boxShadow={useColorModeValue("sm", "sm-dark")}
+      justifyContent="space-between"
       {...boxProps}
     >
       <Stack>
         <Text fontSize="sm" color="muted">
           {label}
         </Text>
-        <Heading size={useBreakpointValue({ base: 'sm', md: 'md' })}>{value}</Heading>
+        <Heading size={useBreakpointValue({ base: "sm", md: "md" })}>
+          {value}
+        </Heading>
       </Stack>
-    </Box>
-  )
-}
 
-export default BaseStatCard
+      <IconButton
+        alignSelf="center"
+        colorScheme="blue"
+        aria-label="Search database"
+        icon={<Icon as={IconSearch} />}
+      />
+
+      {/* <Box alignSelf="center" h="50px" w="50px" 
+      borderRadius="md" bg="blue.500" color="white" >
+        
+      </Box> */}
+    </Flex>
+  );
+};
+
+export default BaseStatCard;
--- app/root/src/pages/dashboard/index.tsx
+++ app/root/src/pages/dashboard/index.tsx
@@ -1,8 +1,5 @@
 import PageContainer from "@/components/PageContainer";
-import {
-  Box,
-  SimpleGrid,
-} from "@chakra-ui/react";
+import { Box, SimpleGrid, useColorModeValue } from "@chakra-ui/react";
 import BaseStarCard from "./BaseStarCard";
 
 const stats = [
@@ -15,6 +12,18 @@ const stats = [
 export default () => {
   return (
     <PageContainer>
+      <Box
+        p={4}
+        mb={4}
+        h="200px"
+        w="full"
+        as="section"
+        bg="white"
+        borderRadius="lg"
+        boxShadow={useColorModeValue("sm", "sm-dark")}
+      >
+        11
+      </Box>
       <Box w="full" as="section">
         <SimpleGrid columns={{ base: 1, md: 4 }} gap={{ base: "5", md: "6" }}>
           {stats.map(({ label, value }) => (
