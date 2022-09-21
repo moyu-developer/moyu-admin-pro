b6d18e09ecc3b23f027595693534288922301a6c
wangly19 <18607948581@163.com>
2022-09-21 23:53:09 +0800

    feat: create Spotlight component design.

---
+++ app/root/src/components/Spotlight/Item.tsx
@@ -0,0 +1,17 @@
+import { Box, Flex, Text } from "@chakra-ui/react"
+import { IconHash, IconBrandTelegram } from "@tabler/icons"
+
+const SpotlightResultItem = () => {
+  return (
+    <Flex bg="teal.500" alignItems="center" w="full" mt={3} px={4} py={2} borderRadius="md" color="white" >
+      <IconHash/>
+      <Box flex={1} px={3} >
+        <Text>1</Text>
+        <Text>2</Text>
+      </Box>
+      <IconBrandTelegram/>
+    </Flex>
+  )
+}
+
+export default SpotlightResultItem
---
+++ app/root/src/components/Spotlight/index.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  Box,
+  Divider,
+  Flex,
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+  VStack,
+} from "@chakra-ui/react";
+import SpotlightResultItem from './Item'
+
+import { IconSearch } from "@tabler/icons";
+
+export interface SpotlightModalInstance {
+  onOpen?: () => void;
+}
+
+const SpotlightModal = React.forwardRef<SpotlightModalInstance>((_, ref) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  React.useImperativeHandle(ref, () => ({
+    onOpen,
+  }));
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} size="2xl" >
+      <ModalOverlay />
+      <ModalContent  >
+        <ModalBody px={4}  >
+          <Flex flexDirection="column" >
+            <Flex h="50px" alignItems="center" gap="12px" >
+              <IconSearch />
+              <Input variant="unstyled"  placeholder="查询弹窗" />
+            </Flex>
+            
+            <VStack flex={1} maxW="60vh" pb={4} display="none" >
+            <Divider/>  
+              <SpotlightResultItem/>
+            </VStack>
+          </Flex>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+});
+
+export default SpotlightModal;
