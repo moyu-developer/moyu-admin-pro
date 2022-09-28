3184a55c020a46f82c0214d35948fd43e66c4ef1
wangly19 <18607948581@163.com>
2022-09-28 23:36:18 +0800

    chore: create user profile card.

--- app/root/src/pages/user/info/index.tsx
+++ app/root/src/pages/user/info/index.tsx
@@ -2,7 +2,7 @@ import {
   Box,
   Button,
   Center,
-  Container,
+  Tag,
   Flex,
   Grid,
   GridItem,
@@ -15,68 +15,94 @@ import {
   TabPanel,
   TabPanels,
   Tabs,
+  List,
+  ListIcon,
+  ListItem,
+  Icon,
 } from "@chakra-ui/react";
 import PageContainer from "@/components/PageContainer";
 import Card from "@/components/Card";
+import { IconBuilding, IconLink, IconMapPin, IconUser } from "@tabler/icons";
 
 const UserInfo = () => {
   return (
     <PageContainer>
-      <Grid templateColumns='repeat(4, 1fr)' gap={6}>
-        <GridItem colSpan={1} w='100%'>
+      <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+        <GridItem colSpan={1} w="100%">
           <Card>
-          <VStack>
-          <Image
-            borderRadius='full'
-            boxSize='150px'
-            src='https://bit.ly/dan-abramov'
-            alt='Dan Abramov'
-          />
-          <Heading size="md">1</Heading>
-          <Text>一句话描述你当前在干什么吧</Text>
-          </VStack>
+            <VStack mb={4}>
+              <Image
+                alignSelf="center"
+                borderRadius="full"
+                boxSize="70%"
+                src="https://avatars.githubusercontent.com/u/45115006?v=4"
+              />
+              <Heading pt={2} size="md">
+                fanghua.wang
+              </Heading>
+              <Text fontSize="sm" color="gray.400">
+                一句话描述你当前在干什么吧
+              </Text>
+            </VStack>
+            <Text>
+              一名前端开发者的仓库，如果觉得不错的话，请记得给您中意的项目点一个明星
+            </Text>
+            <Button my={6} w="full">
+              Edit Profile
+            </Button>
+            <List spacing={3}>
+              <ListItem>
+                <ListIcon as={IconUser} color="blue.500" fontSize="xl" />
+                技术专家
+              </ListItem>
+              <ListItem>
+                <ListIcon as={IconMapPin} fontSize="xl" color="blue.500" />
+                浙江省杭州市
+              </ListItem>
+              <ListItem>
+                <ListIcon as={IconLink} fontSize="xl" color="blue.500" />
+                我的主页
+              </ListItem>
+              <ListItem>
+                <ListIcon as={IconBuilding} fontSize="xl" color="blue.500" />
+                Moyu Devloper
+              </ListItem>
+            </List>
+            <Heading my={4} fontSize="md">
+              标签
+            </Heading>
+            <Flex my={4} flexWrap="wrap" gap={2}>
+              <Tag variant='outline' colorScheme='blue' >Adobe Photoshop</Tag>
+              <Tag variant='outline' colorScheme='blue' >UX/UI</Tag>
+              <Tag variant='outline' colorScheme='blue' >Web Design</Tag>
+              <Tag variant='outline' colorScheme='blue' >React</Tag>
+            </Flex>
           </Card>
         </GridItem>
-        <GridItem colSpan={3} w='100%'>
-          <Card title="Edit" >
-          <Tabs>
-  <TabList>
-    <Tab>One</Tab>
-    <Tab>Two</Tab>
-    <Tab>Three</Tab>
-  </TabList>
+        <GridItem colSpan={3} w="100%">
+          <Card title="Edit">
+            <Tabs>
+              <TabList>
+                <Tab>One</Tab>
+                <Tab>Two</Tab>
+                <Tab>Three</Tab>
+              </TabList>
 
-  <TabPanels>
-    <TabPanel>
-      <p>one!</p>
-    </TabPanel>
-    <TabPanel>
-      <p>two!</p>
-    </TabPanel>
-    <TabPanel>
-      <p>three!</p>
-    </TabPanel>
-  </TabPanels>
-</Tabs>
+              <TabPanels>
+                <TabPanel>
+                  <p>one!</p>
+                </TabPanel>
+                <TabPanel>
+                  <p>two!</p>
+                </TabPanel>
+                <TabPanel>
+                  <p>three!</p>
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
           </Card>
         </GridItem>
       </Grid>
-      {/* <Center h="200px">
-        <Box
-          maxW="4xl"
-          minH="200px"
-          w="full"
-          px={{ base: "4", md: "4" }}
-          py={{ base: "3", md: "3" }}
-          bg="white"
-          borderRadius="lg"
-          boxShadow={useColorModeValue("sm", "sm-dark")}
-          position="relative"
-        >
-          111
-          <Button position="absolute" right="24px" top="24px" >Edit</Button>
-        </Box>
-      </Center> */}
     </PageContainer>
   );
 };
