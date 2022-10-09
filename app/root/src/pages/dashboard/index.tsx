6c449f7cd0fc79891be3f91132d3f925ebddde82
wangly19 <18607948581@163.com>
2022-10-09 23:51:55 +0800

    chore: change dashboard cards padding.

--- app/root/src/pages/dashboard/index.tsx
+++ app/root/src/pages/dashboard/index.tsx
@@ -1,13 +1,21 @@
-import Card from "@/components/Card";
 import PageContainer from "@/components/PageContainer";
-import { Box, Flex, Grid, GridItem, SimpleGrid, useColorModeValue, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Grid,
+  GridItem,
+  Icon,
+  SimpleGrid,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { IconRefresh } from "@tabler/icons";
 import BaseStarCard from "./BaseStarCard";
-import ChartLine from './ChartLine'
+import ChartLine from "./ChartLine";
 import FastEntryCard from "./FastEntryCard";
-import NoticeCard from './NoticeCard'
-import VersionInfo from './VersionInfo'
-import TaskCard from './TaskCard'
-import HeatChartCard from './HeatChartCard'
+import NoticeCard from "./NoticeCard";
+import VersionInfo from "./VersionInfo";
+import TaskCard from "./TaskCard";
+import HeatChartCard from "./HeatChartCard";
 import useThemeModeStyle from "@/hooks/useThemeModeStyle";
 
 const stats = [
@@ -18,11 +26,23 @@ const stats = [
 ];
 
 export default () => {
-
-  const { bg } = useThemeModeStyle()
+  const { bg } = useThemeModeStyle();
 
   return (
-    <PageContainer title="仪表盘" description="描述文案"  >
+    <PageContainer
+      title="仪表盘"
+      description="描述文案"
+      extraNode={
+        <Button
+          colorScheme="teal"
+          size="sm"
+          variant="solid"
+          leftIcon={<Icon as={IconRefresh} fontWeight="bold" fontSize="md" />}
+        >
+          同步数据
+        </Button>
+      }
+    >
       <Box
         mb={4}
         h="350px"
@@ -32,33 +52,44 @@ export default () => {
         borderRadius="lg"
         boxShadow={useColorModeValue("sm", "sm-dark")}
       >
-        <ChartLine/>
+        <ChartLine />
       </Box>
-      <Box w="full" as="section" mb={4} >
-        <SimpleGrid columns={{ base: 4, md: 2, sm: 1, lg: 4 }} gap={{ base: "5", md: "6" }}>
+      <Box w="full" as="section" mb={4}>
+        <SimpleGrid
+          columns={{ base: 4, md: 2, sm: 1, lg: 4 }}
+          gap={{ base: "4", md: "4" }}
+        >
           {stats.map(({ label, value }) => (
             <BaseStarCard key={label} label={label} value={value} />
           ))}
         </SimpleGrid>
       </Box>
-      <Grid templateColumns='repeat(4, 1fr)' gap={{ base: "5", md: "6" }} mb={4}  >
-        <GridItem w='100%'>
-        <FastEntryCard/>
+      <Grid
+        templateColumns="repeat(4, 1fr)"
+        gap={{ base: "4", md: "4" }}
+        mb={4}
+      >
+        <GridItem w="100%">
+          <FastEntryCard />
         </GridItem>
-        <GridItem w='100%'>
-          <VersionInfo/>
+        <GridItem w="100%">
+          <VersionInfo />
         </GridItem>
-        <GridItem colSpan={2} w='100%'>
-          <NoticeCard/>
+        <GridItem colSpan={2} w="100%">
+          <NoticeCard />
         </GridItem>
       </Grid>
 
-      <Grid templateColumns='repeat(4, 1fr)' gap={{ base: "5", md: "6" }} maxH="300px" >
-        <GridItem w='100%'>
-        <TaskCard/>
+      <Grid
+        templateColumns="repeat(4, 1fr)"
+        gap={{ base: "4", md: "4" }}
+        maxH="300px"
+      >
+        <GridItem w="100%">
+          <TaskCard />
         </GridItem>
-        <GridItem colSpan={3} w='100%'>
-          <HeatChartCard/>
+        <GridItem colSpan={3} w="100%">
+          <HeatChartCard />
         </GridItem>
       </Grid>
     </PageContainer>
