c2a449b42c08b45687464e06a99149f3111062a0
wangly19 <18607948581@163.com>
2022-10-09 23:51:29 +0800

    chore: add Breadcrumb Component.

--- app/root/src/components/PageContainer/index.tsx
+++ app/root/src/components/PageContainer/index.tsx
@@ -12,6 +12,12 @@ import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
+  TabPanel,
+  Tab,
+  TabList,
+  TabPanels,
+  Tabs,
+  Divider,
 } from "@chakra-ui/react";
 import SpinnerContainer from "@/components/SpinnerContainer";
 import { Fragment, ReactNode } from "react";
@@ -37,27 +43,23 @@ const PageContainer: React.FC<PageContainerProps> = ({
     return <Fragment>{children}</Fragment>;
   }
 
+  const breadcrumb = window.location.pathname.split('/').filter(v => !!v)
+
   return (
     <Box w="full" h="full" {...boxProps}>
-      <Grid gridTemplateRows={"auto 1fr"} h="full">
+      <Grid gridTemplateRows={"auto auto 1fr"} h="full">
         <Flex pb={4} justifyContent="space-between" alignItems="center">
           <VStack alignItems="flex-start" spacing={1}>
-            <Text color={color} fontWeight="medium" fontSize="2xl">
+            <Text color={color} fontWeight="medium" fontSize="xl">
               {title}
             </Text>
             <Text color={color} mt={1}>
               <Breadcrumb>
-                <BreadcrumbItem>
-                  <BreadcrumbLink href="#">Home</BreadcrumbLink>
-                </BreadcrumbItem>
-
-                <BreadcrumbItem>
-                  <BreadcrumbLink href="#">Docs</BreadcrumbLink>
-                </BreadcrumbItem>
-
-                <BreadcrumbItem isCurrentPage>
-                  <BreadcrumbLink href="#">Breadcrumb</BreadcrumbLink>
-                </BreadcrumbItem>
+                {breadcrumb.map((v) => (
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href="#">{v}</BreadcrumbLink>
+                  </BreadcrumbItem>
+                ))}
               </Breadcrumb>
             </Text>
           </VStack>
